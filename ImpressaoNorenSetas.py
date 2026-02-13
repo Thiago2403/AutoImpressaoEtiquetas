49c37d6db4582c9913359e3db5dae9ace57f1719
Thiago2403 <thiagodelphiso2@gmail.com>
2026-02-13 10:02:15 -0300

    Atualizei algumas inconsistências no código e comentei linha por linha

--- ImpressaoNorenSetas.py
+++ ImpressaoNorenSetas.py
@@ -4,50 +4,57 @@ from win32com.client import constants
 
 intervalo = 10
 
-arquivo = "" #Caminho do arquivo
+arquivo = "" #Caminho do arquivo.
 
-app = wx.App(visible = True) # Deixa o app aberto para consultas 
+app = wx.App(visible = True) # Deixa o app aberto para consultas .
 app.display_alerts = False # 
 
-wb = app.books.open(arquivo) # Abre o arquivo
+wb = app.books.open(arquivo) # Abre o arquivo.
 sheet = wb.sheets.active # pega a planilha ativa no momento e guarda na variavel sheet
-rng = sheet.range("J7") # Pega o conteudo da célula J7
+rng = sheet.range("J7") # Pega o conteudo da célula J7.
 
-def definir_direcao(rua,acao):
-    rua = rua.upper()
-    acao = acao.upper()
+def definir_direcao(rua,acao):.
+    rua = rua.upper() # Deixar maiúsculo
+    acao = acao.upper() # Deixar maiúsculo.
 
     if rua in ["A","C","E"]:
-        return "RIGHT" if acao == "COLETAR" else "LEFT"
+        return "RIGHT" if acao == "COLETAR" else "LEFT" # Retorna Right se a célula nas Ruas [A,C,E] forem Coletar, se for abastecer retorna LEFT..
 
     if rua in ["B","D","F"]:
-        return "LEFT" if acao == "COLETAR" else "RIGHT"
+        return "LEFT" if acao == "COLETAR" else "RIGHT" # Retorna LEFT se a célula nas Ruas [B,D,F] forem Coletar, se for Abastecer retorna RIGHT.
 
-    return None
+    return None # Função sem retorno 
 
 def ajustar_seta(shape,direcao,texto):
     if direcao == "RIGHT":
-        shape.api.AutoShapeType = constants.msoShapeRightArrow
+        shape.api.AutoShapeType = constants.msoShapeRightArrow # Ajusta a seta para direita se a direção = RIGHT.
     else:
-        shape.api.AutoShapeType = constants.msoShapeLeftArrow
+        shape.api.AutoShapeType = constants.msoShapeLeftArrow # Ajusta a seta para a esquerda se a direção = LEFT .
 
-    shape.api.TextFrame2.TextRange.Text = texto
+    shape.api.TextFrame2.TextRange.Text = texto # Atualiza o texto pelo conteúdo da variável texto.
 
 def atualizar_etiquetas(sheet, prefixo , quantidade, direcao):
     for i in range(1, quantidade + 1):
-        shape = sheet.shapes(f"{prefixo}_Seta{i}")
-        ajustar_seta(shape,direcao,str(i))
+        shape = sheet.shapes(f"{prefixo}_Seta{i}")# Busca a seta na planilha -----------------------------     Arrumar
+        ajustar_seta(shape,direcao,str(i)) # Ajusta a seta
     
-    shape_fim = sheet.shapes(f"{prefixo}_SetaFim")
-    ajustar_seta(shape_fim, direcao, "FIM")
 
 print("Impressao Iniciada! Aperte CTRL + C para parar")
 
 try:
     while True : 
-        rua = sheet.range("E16").value
-        acao = sheet.range("B18").value
-        direcao = definir_direcao(rua, acao)
+        rua = sheet.range("E16").value # Célula do endereço
+        rua2 = sheet.range("E42").value # Célula de endereço do segundo numero de série
+
+        acao1_1 = sheet.range("B18").value # Célula de Coleta ou Abastecer da 1° etiqueta
+        acao1_2 = sheet.range("B31").value # Célula de Coleta ou Abastecer da 2° etiqueta
+        acao2_1 = sheet.range("B44").value # Célula de Coleta ou Abastecer da 3° etiqueta
+        acao2_2 = sheet.range("B57").value # Célula de Coleta ou Abastecer da 4° etiqueta
+
+        direcao1_1 = definir_direcao(rua, acao1_1)# Define a direção da seta 1 
+        direcao1_2 = definir_direcao(rua, acao1_2)# Define a direção da seta 2
+        direcao2_1 = definir_direcao(rua2, acao2_1)# Define a direçao da seta 3
+        direcao2_2 = definir_direcao(rua2, acao2_2)# Define a direçãp da seta 4 
 
         maguchi1 = int(sheet.range("F3").value)  # NS1
         contador1 = int(rng.value) 
@@ -61,37 +68,43 @@ try:
 
         # -------- NS1 --------
         if contador1 > maguchi1:
-            terminou_ns1 = True
+            terminou_ns1 = True # Contador para parar os maguchis
         else:
             if contador1 == maguchi1:
-                texto_ns1 = f"{contador1} FIM"
+                texto_ns1 = f"{contador1} FIM" # Adiciona a palavra 'FIM' na ultima seta 
             else:
-                texto_ns1 = str(contador1)
+                texto_ns1 = str(contador1) # Numero do maguchi
 
-            shape_ns1 = sheet.shapes("NS1_Seta")
-            ajustar_seta(shape_ns1, direcao, texto_ns1)
-            sheet.range("J7").value += 1
+            shape1_1 = sheet.shapes("Seta1_1") # Definindo a seta 1
+            shape1_2 = sheet.shapes("Seta2_2") # Definindo a seta 2
+            ajustar_seta(shape1_1, direcao1_1, texto_ns1) # Ajustando a direçao da seta 1
+            ajustar_seta(shape1_2, direcao1_2, texto_ns1) # Ajustando a direção da seta 2
+            sheet.range("N7").value += 1 # Somando o contador de maguchis
 
         # -------- NS2 --------
         if contador2 > maguchi2:
-            terminou_ns2 = True
+            terminou_ns2 = True # Contador para parar os maguchis
         else:
             if contador2 == maguchi2:
-                texto_ns2 = f"{contador2} FIM"
+                texto_ns2 = f"{contador2} FIM" # Adiciona a palavra 'FIM' na ultima seta 
             else:
-                texto_ns2 = str(contador2)
+                texto_ns2 = str(contador2) # Numero do maguchi
 
-            shape_ns2 = sheet.shapes("NS2_Seta")
-            ajustar_seta(shape_ns2, direcao, texto_ns2)
-            sheet.range("J8").value += 1
-
-        # -------- FINAL --------
-        if terminou_ns1 and terminou_ns2:
-            print("Todas as etiquetas impressas")
-            break
+            shape2_1 = sheet.shapes("Seta2_1") # Ajustando a direçao da seta 3
+            shape2_2 = sheet.shapes("Seta2_2") # Ajustando a direção da seta 4
+            ajustar_seta(shape2_1, direcao2_1, texto_ns2) # Ajustando a direçao da seta 3
+            ajustar_seta(shape2_2, direcao2_2, texto_ns2) # Ajustando a direçao da seta 4
+            sheet.range("Q7").value += 1
 
         print("Impressão das Etiquetas Concluída!")
         # sheet.api.PrintOut()
+
+        # -------- FINAL --------
+        if terminou_ns1 and terminou_ns2:
+            print("Maguchis finalizados - avançando o numero de serie")
+            
+            sheet.range("J7").value += 1
+
 
         time.sleep(intervalo)
 
